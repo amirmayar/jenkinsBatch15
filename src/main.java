0819fd9ef8fee237a267cddfc2d56f01dca1e436
amirmayar <122411655+amirmayar@users.noreply.github.com>
2023-05-30 20:16:42 -0400

    Initial Commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <10; i++) {
+            System.out.println("Hello World");
+            Thread.sleep(2000);
+
+        }
+    }
+}
